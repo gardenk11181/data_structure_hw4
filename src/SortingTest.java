e8c6fff84decb440797a8b955074c2379f3dce6d
Jeongwon Kim <55073876+gardenk11181@users.noreply.github.com>
2020-05-12 00:39:42 +0900

    Radix Sort to 2d int array

--- src/SortingTest.java
+++ src/SortingTest.java
@@ -269,19 +269,37 @@ public class SortingTest
 		int d = (int)Math.floor(Math.log10(maxAbs))+1;
 		// 최대 자리수를 찾는 과정 빅세타(n)
 
+//		for(int i=0; i<d; i++) {
+//			MyQueue[] myQueues = new MyQueue[19];
+//			for(int m=0; m<myQueues.length; m++) {
+//				myQueues[m] = new MyQueue();
+//			}
+//			for(int j=0; j<value.length; j++) {
+//				int number = (value[j]/((int)Math.pow(10,i)))%10 + 9;
+//				myQueues[number].add(value[j]);
+//			}
+//			int count=0;
+//			for(int k=0; k<myQueues.length; k++) {
+//				while(!myQueues[k].isEmpty()) {
+//					value[count++] = myQueues[k].poll();
+//				}
+//			}
+//		}
+
+
+		// 2D int array
 		for(int i=0; i<d; i++) {
-			MyQueue[] myQueues = new MyQueue[19];
-			for(int m=0; m<myQueues.length; m++) {
-				myQueues[m] = new MyQueue();
-			}
+			int[][] numbers = new int[19][value.length+1];
 			for(int j=0; j<value.length; j++) {
 				int number = (value[j]/((int)Math.pow(10,i)))%10 + 9;
-				myQueues[number].add(value[j]);
+				int lastIndex = numbers[number][value.length];
+				numbers[number][lastIndex] = value[j];
+				numbers[number][value.length]++;
 			}
 			int count=0;
-			for(int k=0; k<myQueues.length; k++) {
-				while(!myQueues[k].isEmpty()) {
-					value[count++] = myQueues[k].poll();
+			for(int k=0; k<19; k++) {
+				for(int m=0; m<numbers[k][value.length]; m++) {
+					value[count++] = numbers[k][m];
 				}
 			}
 		}
@@ -290,69 +308,69 @@ public class SortingTest
 	}
 }
 
-interface MyQueueInterface {
-	public int size();
-	public boolean isEmpty();
-	public int poll();
-	public int peek();
-	public void add(int num);
-}
-
-class MyNode {
-	private int value;
-	private MyNode nextNode;
-	MyNode(int value){
-		this.value = value;
-	}
-	public void setNext(MyNode nextNode) {
-		this.nextNode = nextNode;
-	}
-	public MyNode getNext() {
-		return nextNode;
-	}
-	public int getValue() {
-		return value;
-	}
-
-}
-
-class MyQueue implements MyQueueInterface {
-	MyNode head = new MyNode(0);
-	@Override
-	public int size() {
-		MyNode first = head;
-		int count=0;
-		while(first.getNext()!=null) {
-			first = first.getNext();
-			count++;
-		}
-		return count;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		if(size()==0) return true;
-		else return false;
-	}
-
-	@Override
-	public int poll() {
-		int tmp = head.getNext().getValue();
-		head.setNext(head.getNext().getNext());
-		return tmp;
-	}
-
-	@Override
-	public int peek() {
-		return head.getNext().getValue();
-	}
-
-	@Override
-	public void add(int num) {
-		MyNode lastNode = head;
-		for(int i=0; i<size(); i++) {
-			lastNode = lastNode.getNext();
-		}
-		lastNode.setNext(new MyNode(num));
-	}
-}
+//interface MyQueueInterface {
+//	public int size();
+//	public boolean isEmpty();
+//	public int poll();
+//	public int peek();
+//	public void add(int num);
+//}
+//
+//class MyNode {
+//	private int value;
+//	private MyNode nextNode;
+//	MyNode(int value){
+//		this.value = value;
+//	}
+//	public void setNext(MyNode nextNode) {
+//		this.nextNode = nextNode;
+//	}
+//	public MyNode getNext() {
+//		return nextNode;
+//	}
+//	public int getValue() {
+//		return value;
+//	}
+//
+//}
+//
+//class MyQueue implements MyQueueInterface {
+//	MyNode head = new MyNode(0);
+//	@Override
+//	public int size() {
+//		MyNode first = head;
+//		int count=0;
+//		while(first.getNext()!=null) {
+//			first = first.getNext();
+//			count++;
+//		}
+//		return count;
+//	}
+//
+//	@Override
+//	public boolean isEmpty() {
+//		if(size()==0) return true;
+//		else return false;
+//	}
+//
+//	@Override
+//	public int poll() {
+//		int tmp = head.getNext().getValue();
+//		head.setNext(head.getNext().getNext());
+//		return tmp;
+//	}
+//
+//	@Override
+//	public int peek() {
+//		return head.getNext().getValue();
+//	}
+//
+//	@Override
+//	public void add(int num) {
+//		MyNode lastNode = head;
+//		for(int i=0; i<size(); i++) {
+//			lastNode = lastNode.getNext();
+//		}
+//		lastNode.setNext(new MyNode(num));
+//	}
+//}
